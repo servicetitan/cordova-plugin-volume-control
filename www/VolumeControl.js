ccc1bd03cce7081d7333e4b00090adc349e91c08
Marat Faskhiev <marat.faskhiev@gmail.com>
2018-12-04 16:42:25 -0800

    MOB-136: remove Non-public API usage

--- www/VolumeControl.js
+++ www/VolumeControl.js
@@ -10,23 +10,8 @@ function defaults(object, source) {
   return object;
 }
 
-exports.toggleMute = function( success, error) {
-  exec(success, error, 'VolumeControl', 'toggleMute', []);
-};
-
-exports.isMuted = function(success, error) {
-  exec(success, error, 'VolumeControl', 'isMuted', []);
-};
-
 exports.getVolume = function(success, error) {
   exec(success, error, 'VolumeControl', 'getVolume', []);
-};
-
-exports.setVolume = function(volume, success, error) {
-  if (volume > 1) {
-    volume /= 100;
-  }
-  exec(success, error, 'VolumeControl', 'setVolume', [volume * 1]);
 };
 
 /*
